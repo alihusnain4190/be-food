bcbda4a982eb2c800cc6fe4e92123c1224543c84
alihusnain4190 <alihusnain4190@gmail.com>
2021-04-14 12:47:02 +0100

    changes

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
-const ENV = process.env.NODE_ENV || "development";
-const connection = require("knex");
+
+const ENV = process.env.NODE_ENV || 'development';
+const knex = require('knex');
 
 const dbConfig =
-  ENV === "production"
-    ? { client: "pg", connection: process.env.DATABASE_URL }
-    : require("../Knexfile");
-console.log(ENV);
-module.exports = connection(dbConfig);
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
+
+module.exports = knex(dbConfig);
