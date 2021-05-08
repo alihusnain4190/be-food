f6650efea4facab4d367efdb73f97532a02e9f8a
alihusnain4190 <alihusnain4190@gmail.com>
2021-05-08 01:13:23 +0100

    add pizza imges

--- db/data/devData/pizza.js
+++ db/data/devData/pizza.js
@@ -1,35 +1,88 @@
 const pizza = [
   {
-    p_image:
-      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1314&q=80",
-    p_name: "BBQ-chicken",
+    p_image: "https://www.papajohns.co.uk/images/pizzas/premium_hawaiian.jpg",
+    p_name: "premium_hawaiian",
 
-    p_larg: 15,
-    p_small: 10,
-    p_medium: 12,
+    p_small: 14.49,
+    p_medium: 16.49,
+    p_larg: 18.49,
 
     dip: "Garlic dip",
   },
   {
-    p_name: "Meat Feast",
+    p_name: "american_hot",
 
-    p_image:
-      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1314&q=80",
+    p_image: "https://www.papajohns.co.uk/images/pizzas/american_hot_menu.jpg",
 
-    p_larg: 15,
-    p_small: 10,
-    p_medium: 12,
+    p_small: 14.49,
+    p_medium: 16.49,
+    p_larg: 18.49,
+
     dip: "Garlic dip",
   },
   {
-    p_name: "BBQ-chicken",
+    p_name: "Sausage & Pepperoni",
 
     p_image:
-      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1314&q=80",
+      "https://www.papajohns.co.uk/images/pizzas/the-papas-favourite.jpg",
 
-    p_larg: 15,
-    p_small: 10,
-    p_medium: 12,
+    p_small: 16.49,
+    p_medium: 18.49,
+    p_larg: 20,
+    dip: "Garlic dip",
+  },
+  {
+    p_name: "BBQ Chicken Classic",
+
+    p_image: "https://www.papajohns.co.uk/images/pizzas/BBQChickenClassic.jpg",
+
+    p_small: 15.49,
+    p_medium: 17.49,
+    p_larg: 19.49,
+    dip: "Garlic dip",
+  },
+  {
+    p_name: "Amarican Hot",
+
+    p_image:
+      "https://www.papajohns.co.uk/images/pizzas/American-Hottest-Pizza.jpg",
+
+    p_small: 14.49,
+    p_medium: 16.49,
+    p_larg: 18.49,
+    dip: "Garlic dip",
+  },
+  {
+    p_name: "Hawaiian",
+
+    p_image:
+      "https://www.papajohns.co.uk/images/pizzas/premium_hawaiian.jpg",
+
+    p_small: 16.49,
+    p_medium: 18.49,
+    p_larg: 20,
+    dip: "Garlic dip",
+  },
+  {
+    p_name: "Garden party",
+
+    p_image:
+      "https://www.papajohns.co.uk/images/pizzas/garden-party.jpg",
+
+    p_small: 16.49,
+    p_medium: 18.49,
+    p_larg: 20,
+    dip: "Garlic dip",
+  },
+  {
+    p_name: "Cheese and Tomato",
+
+    p_image:
+      "https://www.papajohns.co.uk/images/pizzas/cheese-and-tomato.jpg",
+
+    p_small: 16.49,
+    p_medium: 18.49,
+    p_larg: 20,
     dip: "Garlic dip",
   },
 ];
